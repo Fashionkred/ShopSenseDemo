c850e312041ff0f2016e4547f6e77c9d9927309d
nirveek <nirveek@gmail.com>
2013-09-15 23:58:04 -0700

    Adding tag.cs file

---
+++ ShopSenseDemo/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopSenseDemo
+{
+    public class Tag
+    {
+        public long id { get; set; }
+
+        public string name { get; set; }
+
+        public Tag(long tagId, string tagName)
+        {
+            id = tagId;
+            name = tagName;
+        }
+    }
+}
